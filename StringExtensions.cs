ffa2969e99dba00b969ad9ecbaccd95e5083977a
Brian Ostrander <ostrander.brian@gmail.com>
2016-09-14 00:46:55 -0400

    Added truncation.

--- StringExtensions.cs
+++ StringExtensions.cs
@@ -6,5 +6,11 @@
 		{
 			return string.IsNullOrEmpty(value) || value.Trim().Length == 0;
 		}
+
+		public static string TruncateStart(string value, int length, string truncation = "...")
+		{
+			if (value.Length <= length) return value;
+			return truncation + value.Substring(value.Length - length);
+		}
 	}
 }
