955bb2a567965899b982388e5a50e8072128a350
Brian Ostrander <Ostrander.Brian@gmail.com>
2016-09-20 17:21:02 -0400

    Raw serialization.

--- Serialization.cs
+++ Serialization.cs
@@ -1,8 +1,10 @@
-﻿using UnityEngine;
-using System;
+﻿using System;
+using System.Linq;
+using UnityEngine;
 using Newtonsoft.Json;
-using LunraGames.Converters;
+using Newtonsoft.Json.Linq;
 using Newtonsoft.Json.Converters;
+using LunraGames.Converters;
 
 namespace LunraGames
 {
@@ -85,6 +87,35 @@ namespace LunraGames
 		public static string SerializeJson(object value, bool verbose = false)
 		{
 			return value == null ? string.Empty : JsonConvert.SerializeObject(value, Formatting.None, verbose ? VerboseSerializerSettings : SerializerSettings);
+		}
+
+		/// <summary>
+		/// Deserializes the json into a raw dictionary of strings and objects.
+		/// </summary>
+		/// <returns>The dictionary of key value pairs.</returns>
+		/// <param name="json">Json.</param>
+		/// <param name="defaultValue">Default value.</param>
+		/// <param name="verbose">Verbose.</param>
+		public static object DeserializeJsonRaw(string json, object defaultValue = null, bool verbose = false)
+		{
+			if (StringExtensions.IsNullOrWhiteSpace(json)) return defaultValue;
+
+			try { return DeserializeRaw(JToken.Parse(json)); }
+			catch (Exception e)
+			{
+				Debug.LogError("Problem parsing value: \n\t" + json + "\nReturning default value\n Exception:\n" + e.Message);
+				return defaultValue;
+			}
+		}
+
+		static object DeserializeRaw(JToken token)
+		{
+			switch (token.Type)
+			{
+				case JTokenType.Object: return token.Children<JProperty>().ToDictionary(p => p.Name, p => DeserializeRaw(p.Value));
+				case JTokenType.Array: return token.Select(t => DeserializeRaw(t)).ToList();
+				default: return ((JValue)token).Value;
+			}
 		}
 	}
 }
