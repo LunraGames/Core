ad1d0b785011d83eda1cc1dc1a4376ee650dddfa
Brian Ostrander <Ostrander.Brian@gmail.com>
2016-10-03 00:28:01 -0400

    Added plugin guard.

---
+++ Editor/LunraGamesPluginGuard.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace LunraGames
+{
+	public static class PluginGuard
+	{
+		static bool? _IsInPluginsDirectory;
+
+		public static bool IsInPluginsDirectory
+		{ 
+			get 
+			{
+				if (_IsInPluginsDirectory.HasValue) return _IsInPluginsDirectory.Value;
+
+				var assets = AssetDatabase.FindAssets("t:Script LunraGamesPluginGuard");
+				if (assets == null || assets.Length == 0) return false;
+				var path = AssetDatabase.GUIDToAssetPath(assets[0]);
+				return (_IsInPluginsDirectory = path.ToLower().Contains("/plugins/")).Value;
+			} 
+		}
+	}
+}
